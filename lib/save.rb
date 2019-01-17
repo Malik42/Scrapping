a3b04fc084b3fce748997432aa2f537a3810330f
Malik42 <malik.ahres@hotmail.fr>
2019-01-17 21:55:15 +0100

    test unitaire

---
+++ lib/save.rb
@@ -0,0 +1,47 @@
+require 'nokogiri'
+require 'open-uri'
+
+def get_url_and_name
+  page_url = "https://www.nosdeputes.fr/deputes"
+  doc = Nokogiri::HTML(open(page_url))
+  annuaire = doc.css("span.list_nom")
+  name_and_url = []
+
+  annuaire.map.with_index do |element, i|
+    nom_prenom = element.text.tr("\n", "").split(",")
+    taille = nom_prenom[1].length
+    nom_prenom[1][taille - 4..taille] = ''
+    nom_prenom[1][0] = ''
+    nom_prenom[0][0..5] = ''
+    if nom_prenom[0] == "Mörch"
+      nom_prenom[0] = "moerch"
+    end
+    name_and_url << {"nom" => nom_prenom[0], "prenom" => nom_prenom[1], "url" => "https://www.nosdeputes.fr/" + nom_prenom[1].downcase.tr(" ", "-") + "-" + nom_prenom[0].downcase.tr(" ", "-")}
+  end
+  name_and_url
+end
+
+def get_townhall_email(townhall_url)
+  page_url = townhall_url
+  doc = Nokogiri::HTML(open(page_url))
+  email = doc.xpath("//a[contains(@href,'mailto')]").first.text
+end
+
+def get_all_email(deputes)
+  email_tab = []
+  deputes.map.with_index do |depute, index|
+    url = depute["url"].tr(
+      "'ÀÁÂÃÄÅàáâãäåĀāĂăĄąÇçĆćĈĉĊċČčÐðĎďĐđÈÉÊËèéêëĒēĔĕĖėĘęĚěĜĝĞğĠġĢģĤĥĦħÌÍÎÏìíîïĨĩĪīĬĭĮįİıĴĵĶķĸĹĺĻļĽľĿŀŁłÑñŃńŅņŇňŉŊŋÒÓÔÕÖØòóôõöøŌōŎŏŐőŔŕŖŗŘřŚśŜŝŞşŠšſŢţŤťŦŧÙÚÛÜùúûüŨũŪūŬŭŮůŰűŲųŴŵÝýÿŶŷŸŹźŻżŽž",
+      "-AAAAAAaaaaaaAaAaAaCcCcCcCcCcDdDdDdEEEEeeeeEeEeEeEeEeGgGgGgGgHhHhIIIIiiiiIiIiIiIiIiJjKkkLlLlLlLlLlNnNnNnNnnNnOOOOOOooooooOoOoOoRrRrRrSsSsSsSssTtTtTtUUUUuuuuUuUuUuUuUuUuWwYyyYyYZzZzZz")
+    email = get_townhall_email(url)
+    email_tab << {"first_name" => depute["nom"], "last_name" => depute["prenom"], "email" => email}
+  end
+  email_tab
+end
+
+def perform
+  depute = get_all_email(get_url_and_name)
+  puts depute
+end
+
+perform
---
+++ spec/depute_scrap_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../lib/depute_scrap'
+
+describe 'get url and name' do
+  it "return a table with the name and surname of the members and their email" do
+    expect(get_url_and_name).to be_instance_of Array
+  end
+end
+
+describe 'get_townhall_email' do
+  it "return the email that was taken on the website by the url" do
+    expect(get_townhall_email).to eq("contact@damien-abad.fr")
+  end
+end
