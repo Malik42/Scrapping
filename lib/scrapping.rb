a7f185ab1090b1c5ffc5dcb5664bf7d5be710bbc
Amine42 <amine.bouraghda@gmail.com>
2019-01-17 13:53:54 +0100

    get crypto name

--- lib/scrapping.rb
+++ lib/scrapping.rb
@@ -1,17 +1,39 @@
 require 'nokogiri'
 require 'open-uri'
 
-Page_Url = 'https://coinmarketcap.com/all/views/all/'
-doc = Nokogiri::HTML(open(Page_Url))
-# //*[@id="id-bitcoin"]/td[2]
-# node_text = doc.xpath('//*[@id="currencies-all"]/tbody')..text
-# node_text = doc.xpath('//*[@id="currencies-all"]/tbody//td').text
-node_text = doc.css("a[href].currency-name-container")
-  # node_text = node.text
-  # puts node_text
-  # node_text = doc.css("a.price")
+def merge_tab_in_hash(array_key, array_value)
+  hash = {}
+  
+  array_key.zip(array_value) { |key, value| hash[key] = value }
+  # tab = [hash]
+  hash
+end
 
-  # puts node_text['href'].byteslice(12, 7)
-  node_text.map do |element|
-    puts element["href"].byteslice(12, element["href"].length).tr("/", "")
+def scrap_crypto
+  page_url = "https://coinmarketcap.com/all/views/all/"
+  doc = Nokogiri::HTML(open(page_url))
+  name = doc.css("span/a.link-secondary")
+  price = doc.css("a.price")
+  price_tb = []
+  name_tb = []
+
+  name.map.with_index do |element, i|
+    name_tb[i] = element.text
   end
+
+  price.map.with_index do |element, i|
+    price_tb[i] = element.text
+  end
+
+  final_hash = merge_tab_in_hash(name_tb, price_tb)
+end
+
+def perform
+  crypto = scrap_crypto
+  # puts crypto
+  for i in 0..crypto
+    puts i
+  end
+end
+
+perform
