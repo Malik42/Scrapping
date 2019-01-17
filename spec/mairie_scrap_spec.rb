ac9adabf0260b27c9819df4919865dc489448da3
Amine42 <amine.bouraghda@gmail.com>
2019-01-17 23:41:40 +0100

    mairie spec

--- spec/mairie_scrap_spec.rb
+++ spec/mairie_scrap_spec.rb
@@ -3,6 +3,8 @@ require_relative '../lib/mairie_scrap'
 describe 'get_url_and_name' do
   it "return a table with the city the mairie website url" do
     expect(get_url_and_name).to be_instance_of Array
+    expect(get_url_and_name).to include({"ville" => "BANTHELU", "url" => "http://annuaire-des-mairies.com/95/banthelu.html"})
+    expect(get_url_and_name).to include({"ville" => "BUHY", "url" => "http://annuaire-des-mairies.com/95/buhy.html"})
   end
 end
 
@@ -16,8 +18,9 @@ describe 'get_townhall_email' do
 end
 
 describe 'get_all_email' do
-  it "return the email that was taken on the website by the url" do
+  it "return array with the name of the city and his email" do
     expect(get_all_email([{"ville" => "BUHY", "url" => "http://annuaire-des-mairies.com/95/buhy.html"}])).to be_instance_of Array
     expect(get_all_email([{"ville" => "BUHY", "url" => "http://annuaire-des-mairies.com/95/buhy.html"}])).to include({"BUHY" => "mairie.buhy@wanadoo.fr"})
+    expect(get_all_email([{"ville" => "VEMARS", "url" => "http://annuaire-des-mairies.com/95/vemars.html"}])).to include({"VEMARS" => "sandrine_ravenel@yahoo.fr"})
   end
 end
