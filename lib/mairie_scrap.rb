a2f0b4d4090753afee87a3a5b08db3dd777d4cd0
Amine42 <amine.bouraghda@gmail.com>
2019-01-17 15:08:13 +0100

    mairie tab

--- lib/mairie_scrap.rb
+++ lib/mairie_scrap.rb
@@ -1,9 +1,22 @@
 require 'nokogiri'
 require 'open-uri'
 
-page_url = "http://annuaire-des-mairies.com/val-d-oise.html"
-doc = Nokogiri::HTML(open(page_url))
+def get_url_and_name
+  page_url = "http://annuaire-des-mairies.com/val-d-oise.html"
+  doc = Nokogiri::HTML(open(page_url))
+  ville = doc.css("a[href].lientxt")
+  # puts ville["href"]
+  # name_and_url = []
 
-ville = doc.css("a.lientxt")
-#email = doc.css("a.price")
-puts ville
+  ville.map.with_index do |element, i|
+  #   name_and_url[i][0] = element.text
+  #   name_and_url[i][1] = element["href"]
+    puts element.text
+  end
+
+  # name_and_url
+end
+
+get_url_and_name
+# #email = doc.css("a.price")
+# puts ville.text.split
