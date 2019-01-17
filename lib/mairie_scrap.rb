013724853a08e4cec0b7ef93aef7e00a38c046ff
Amine42 <amine.bouraghda@gmail.com>
2019-01-17 15:28:49 +0100

    first methode

--- lib/mairie_scrap.rb
+++ lib/mairie_scrap.rb
@@ -6,17 +6,17 @@ def get_url_and_name
   doc = Nokogiri::HTML(open(page_url))
   ville = doc.css("a[href].lientxt")
   # puts ville["href"]
-  # name_and_url = []
+  name_and_url = {}
 
   ville.map.with_index do |element, i|
-  #   name_and_url[i][0] = element.text
-  #   name_and_url[i][1] = element["href"]
-    puts element.text
+    name_and_url[i] = "name" => element.text
+    # name_and_url[i]["url"] = element["href"]
+    # puts element.text
   end
 
-  # name_and_url
+  name_and_url
 end
 
-get_url_and_name
+puts get_url_and_name
 # #email = doc.css("a.price")
 # puts ville.text.split
