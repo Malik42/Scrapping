0fb64929172fcaebf925a43c796acd2935f179c4
Malik42 <malik.ahres@hotmail.fr>
2019-01-17 13:56:09 +0100

    test

--- lib/scrapping.rb
+++ lib/scrapping.rb
@@ -1,17 +1,13 @@
 require 'nokogiri'
 require 'open-uri'
 
+crypto ={}
+
 Page_Url = 'https://coinmarketcap.com/all/views/all/'
 doc = Nokogiri::HTML(open(Page_Url))
-# //*[@id="id-bitcoin"]/td[2]
-# node_text = doc.xpath('//*[@id="currencies-all"]/tbody')..text
-# node_text = doc.xpath('//*[@id="currencies-all"]/tbody//td').text
-node_text = doc.css("a[href].currency-name-container")
-  # node_text = node.text
-  # puts node_text
-  # node_text = doc.css("a.price")
 
-  # puts node_text['href'].byteslice(12, 7)
+node_text = doc.css("a[href].currency-name-container")
   node_text.map do |element|
-    puts element["href"].byteslice(12, element["href"].length).tr("/", "")
-  end
+  crypto = {"name" => element["href"].byteslice(12, element["href"].length).tr("/", "")}
+  puts crypto["name"]
+end
