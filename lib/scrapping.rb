9d4fafdeb5ba03c261fa8c46793d1991efa5ff62
Malik42 <malik.ahres@hotmail.fr>
2019-01-17 11:13:26 +0100

    crypto recovery program add

--- lib/scrapping.rb
+++ lib/scrapping.rb
@@ -1,2 +1,9 @@
 require 'nokogiri'
 require 'open-uri'
+
+Page_Url = 'https://coinmarketcap.com/all/views/all/'
+doc = Nokogiri::HTML(open(Page_Url))
+
+doc.xpath('//*[@id="currencies-all"]/tbody').each do |node|
+  puts node.text
+end
